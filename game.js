2a6e6f4b5c8e46a316676122cdda5d7d0574c493
Maximo <maximodleon@gmail.com>
2019-02-20 20:01:14 -0400

    poner tiles al azar. crear metodos de movimiento de los tiles

--- game.js
+++ game.js
@@ -5,8 +5,16 @@ let gameOptions = {
   boardSize: {
     rows: 4,
     cols: 4
-  }
+  },
+  tweenSpeed: 2000,
+  swipeMaxTime: 1000,
+  swipeMinDistance: 20,
+  swipeMinNormal: 0.85
 }
+const LEFT = 0;
+const RIGHT = 1;
+const UP = 2;
+const DOWN = 3;
 
 window.onload = function() {
  let config = {
@@ -45,12 +53,106 @@ class PlayGame extends Phaser.Scene {
    }
 
     create() {
+      this.boardArray = []
+      this.canMove = false
+      this.input.keyboard.on('keydown', this.handleKey, this)
+      this.input.on('pointerup', this.handleSwipe, this)
       for (let i=0; i < gameOptions.boardSize.rows; i++) {
+       this.boardArray[i] = []
        for (let j=0; j < gameOptions.boardSize.cols; j++) {
            let tilePosition = this.getTilePosition(i, j)
            this.add.image(tilePosition.x, tilePosition.y, 'emptytile')
+           let tile =this.add.sprite(tilePosition.x, tilePosition.y, 'tiles', 0)
+           tile.visible = false
+           this.boardArray[i][j] = { tileValue: 0, tileSprite: tile }
           }
         }
+        this.addTile()
+        this.addTile()
+    }
+
+    addTile() {
+      let emptyTiles = []
+      for(let i = 0; i < gameOptions.boardSize.rows; i++){
+         for(let j = 0; j < gameOptions.boardSize.cols; j++) {
+            if(this.boardArray[i][j].tileValue === 0) {
+              emptyTiles.push({ row: i, col: j })
+            }
+         }
+      }
+
+      if(emptyTiles.length > 0) {
+        let chosenTile = Phaser.Utils.Array.GetRandom(emptyTiles)
+        this.boardArray[chosenTile.row][chosenTile.col].tileValue = 1
+        this.boardArray[chosenTile.row][chosenTile.col].tileSprite.visible = true
+        this.boardArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0)
+        this.boardArray[chosenTile.row][chosenTile.col].tileSprite.alpha = 0
+        this.tweens.add({
+             targets: [this.boardArray[chosenTile.row][chosenTile.col].tileSprite],
+             alpha: 1,
+             duration: gameOptions.tweenSpeed,
+             callbackScope: this,
+             onComplete: () => {
+               console.log('tween complete')
+               this.canMove = true
+             }
+        })
+      }
+    }
+
+    makeMove(direction) {
+       console.log('about to move')
+    }
+
+    handleKey(event) {
+       if(this.canMove) {
+         switch(event.code) {
+             case 'KeyA':
+             case 'ArrowLeft':
+                this.makeMove(LEFT)
+                break;
+             case 'KeyD':
+             case 'ArrowRight':
+                this.makeMove(LEFT)
+                break;
+             case 'KeyW':
+             case 'ArrowUp':
+                this.makeMove(LEFT)
+                break;
+             case 'KeyS':
+             case 'ArrowDown':
+                this.makeMove(LEFT)
+                break;
+         }
+       }
+    }
+
+    handleSwipe(event) {
+       if(this.canMove) {
+         let swipeTime = event.upTime - event.downTime
+         let fastEnough = swipeTime < gameOptions.swipeTimeMax
+         let swipe = new Phaser.Geom.Point(event.upX - event.downX, event.upY - event.upX)
+         let swipeMagnitude = Phaser.Geom.Point.GetMagnitude(swipe)
+         let longEnough = swipeMagnitude > gameOptions.swipeMinDistance
+
+         if(longEnough && fastEnough) {
+           Phaser.Geom.Point.SetMagnitude(swipe, 1)
+           if(swipe.x > gameOptions.swipeMinNormal) {
+              this.makeMove(RIGHT)
+           }
+           if(swipe.x < -gameOptions.swipeMinNormal) {
+              this.makeMove(LEFT)
+           }
+
+           if(swipe.y > gameOptions.swipeMinNormal) {
+              this.makeMove(DOWN)
+           }
+           if(swipe.y < -gameOptions.swipeMinNormal){
+              this.makeMove(UP)
+           }
+
+         }
+       }
     }
 
     getTilePosition(row, col) {
@@ -69,6 +171,10 @@ class BootGame extends Phaser.Scene {
 
   preload(){
    this.load.image('emptytile', 'assets/sprites/emptytile.png')
+   this.load.spritesheet('tiles', 'assets/sprites/tiles.png', {
+       frameWidth: gameOptions.tileSize,
+       frameHeight: gameOptions.tileSize
+   })
   }
 
   create() {
