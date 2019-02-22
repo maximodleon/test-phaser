931f25f18110aa85f49740e1b581f4e5e8a72a27
Maximo <maximodleon@gmail.com>
2019-02-21 21:09:22 -0400

    agregar logica para hacer movimientos de los tiles

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ let gameOptions = {
     rows: 4,
     cols: 4
   },
-  tweenSpeed: 2000,
+  tweenSpeed: 200,
   swipeMaxTime: 1000,
   swipeMinDistance: 20,
   swipeMinNormal: 0.85
@@ -100,9 +100,87 @@ class PlayGame extends Phaser.Scene {
       }
     }
 
-    makeMove(direction) {
-       console.log('about to move')
+    makeMove(d) {
+       // if moving left or right, do not need to change 
+       // rows, so we return 0 in each case, else
+       // we add one to move down and minus 1 to move up
+       // same logic for moving left or right
+       let dRow = (d === LEFT || d === RIGHT) ? 0 : d === UP ? -1 : 1
+       let dCol = (d === UP || d === DOWN) ? 0 : d === LEFT ? -1 : 1
+       this.canMove = false
+       let movedTiles = 0
+
+       // this code is to avoid moving
+       // tiles at the edge of the board
+       // by setting the firstRow or firstCol to
+       // 1 we avoid moving tiles in those rows/cols
+       // because we loop only to get tiles 'inside' the board
+       let firstRow = (d === UP) ? 1 : 0
+       let lastRow = gameOptions.boardSize.rows - ((d === DOWN) ? 1 : 0)
+       let firstCol = (d === LEFT) ? 1 : 0
+       let lastCol = gameOptions.boardSize.cols - ((d === RIGHT) ? 1 : 0)
+
+       for (let i=firstRow; i < lastRow; i++){
+           for (let j=firstCol; j < lastCol; j++) {
+               let curRow = dRow === 1 ? (lastRow - 1) - i : i
+               let curCol = dCol === 1 ? (lastCol - 1) - j : j
+               let tileValue = this.boardArray[curRow][curCol].tileValue
+
+               if (tileValue !== 0) {
+                 let newRow = curRow
+                 let newCol = curCol
+                 while(this.isLegalPosition(newRow + dRow, newCol + dCol)) {
+                    newRow += dRow
+                    newCol += dCol
+                 }
+
+                 movedTiles++
+                 this.boardArray[curRow][curCol].tileSprite.depth = movedTiles
+                 let newPos = this.getTilePosition(newRow, newCol)
+                 this.boardArray[curRow][curCol].tileSprite.x = newPos.x
+                 this.boardArray[curRow][curCol].tileSprite.y = newPos.y
+                 this.boardArray[curRow][curCol].tileValue = 0
+
+                 if(this.boardArray[newRow][newCol].tileValue === tileValue) {
+                   this.boardArray[newRow][newCol].tileValue++
+                   this.boardArray[curRow][curCol].tileSprite.setFrame(tileValue)
+                 } else {
+                   this.boardArray[newRow][newCol].tileValue = tileValue
+                 }
+               }
+            }
+        }
+
+        this.refreshBoard()
     }
+
+    isLegalPosition(row, col) {
+      let rowInside = row >= 0 && row < gameOptions.boardSize.rows
+      let colInside = col >= 0 && col < gameOptions.boardSize.cols
+
+      return rowInside && colInside
+    }
+
+    refreshBoard() {
+      for(let i=0; i < gameOptions.boardSize.rows; i++) { 
+         for (let j=0; j < gameOptions.boardSize.cols; j++) {
+           let spritePosition = this.getTilePosition(i, j)
+           this.boardArray[i][j].tileSprite.x = spritePosition.x
+           this.boardArray[i][j].tileSprite.y = spritePosition.y
+           let tileValue = this.boardArray[i][j].tileValue
+
+           if(tileValue > 0) {
+             this.boardArray[i][j].tileSprite.visible = true
+             this.boardArray[i][j].tileSprite.setFrame(tileValue - 1)
+
+           } else {
+             this.boardArray[i][j].tileSprite.visible = false
+           }
+         }
+      }
+     this.addTile()
+    }
+
 
     handleKey(event) {
        if(this.canMove) {
@@ -113,15 +191,15 @@ class PlayGame extends Phaser.Scene {
                 break;
              case 'KeyD':
              case 'ArrowRight':
-                this.makeMove(LEFT)
+                this.makeMove(RIGHT)
                 break;
              case 'KeyW':
              case 'ArrowUp':
-                this.makeMove(LEFT)
+                this.makeMove(UP)
                 break;
              case 'KeyS':
              case 'ArrowDown':
-                this.makeMove(LEFT)
+                this.makeMove(DOWN)
                 break;
          }
        }
